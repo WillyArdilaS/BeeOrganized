abfeccb47da88326b045547d286cf3a7b70f9c53
WillyArdilaS <williamardilasanchez@gmail.com>
2022-07-26 21:46:49 -0500

    JS structure

---
+++ JS/Helpers/OwnerDropdown.js
@@ -0,0 +1,25 @@
+import { owners } from "./OwnerList.js";
+
+let selectHTML = document.getElementById("taskOwner");
+
+const getOwner = (pos)=>{
+    return new Promise((resolve, reject)=>{
+        let owner = owners[pos];
+
+        (owner) ? resolve(owner) : reject("");
+    });
+}
+
+const showOwner = async ()=> {
+    let ownerList = [];
+
+    for(let i = 0; i < owners.length; i++) {
+        ownerList[i] = await getOwner(i);
+
+        let optionHTML = `<option value="${ownerList[i].ownerName}"> ${ownerList[i].ownerName} </option>`
+
+        selectHTML.innerHTML += optionHTML;
+    }
+}
+
+showOwner();
